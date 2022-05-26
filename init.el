f89fe5256ea7050746ff44229ef5949547db5906
guanke.sun <guanke.sun@horizon.ai>
2022-05-26 15:22:58 +0800

    取消vim mode

--- init.el
+++ init.el
@@ -17,7 +17,6 @@
 
 (require 'init-folding)
 
-(require 'init-vim)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
